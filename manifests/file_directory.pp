6c3aaa27b1f0b76b3dc4f4943858942c8f38eced
nasir.mahmood@gmail.com <nasir.mahmood@gmail.com>
2019-10-10 07:57:39 -0400

    Deliver to all -- local-files

--- manifests/file_directory.pp
+++ manifests/file_directory.pp
@@ -2,5 +2,7 @@ file { '/etc/config_dir':
   ensure => directory,
   owner => 'nmr',
   group => 'nmr',
- mode => '777',
+  mode => '777',
+  source => '/root/puppet/files/config_dir',
+  recurse => 'true',
 }
