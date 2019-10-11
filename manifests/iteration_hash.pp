d99a0cfa0923b24bad0480404efe8b5ea1bf51d4
nasir.mahmood@gmail.com <nasir.mahmood@gmail.com>
2019-10-11 08:09:02 -0400

    Deliver to all -- local-files

--- manifests/iteration_hash.pp
+++ manifests/iteration_hash.pp
@@ -1,4 +1,4 @@
 $nics = $facts['networking']['interfaces']
 $nics.each | String $interface, Hash $attribute | { 
-notice("Inetfaces ${interface} has an IP address ${attribute['ip']}")
+notice("Inetfaces ${interface} has an IP address ${attribute['ip']},${attribute['netmask']}")
 }
